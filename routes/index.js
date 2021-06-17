56f7cbd3320c134eb898f0474a459e050cf74ee9
haamseongho <haams704@gmail.com>
2021-06-17 23:14:49 +0900

    간단한 내용 추가

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,14 @@ const uuid4 = require("uuid4");
 const myVar = require("./myvar");
 const setVar = new myVar(); // require로 가지고오면 객체생성해서 써야함
 router.get("/",(req,res)=>{
-    if(로그인X){
-        res.redirect("/login")
-    }
     res.render("main/index.html",{
-        message : "hi what's your name?11" + setVar.name,
+        message : "메인페이지" + setVar.name,
         online : "<h1>태그가 출력됩니다.</h1>"
     })
+})
+
+router.get("/login",(req,res) =>{
+    res.render("login/signup.html")
 })
 
 module.exports = router;
